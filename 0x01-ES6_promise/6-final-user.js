12eb861a33e0e12e87892dab7db6ee1ddf09520a
Nevillionaire <75064256+nevillionaire@users.noreply.github.com>
2022-12-22 21:05:21 +0300

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,14 +2,21 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((values) => {
-    const arr = [];
-    for (const item of values) {
-      arr.push({ status: item.status, value: item.value || item.reason });
-    }
-    return arr;
+  const signUp = signUpUser(firstName, lastName);
+  const upload = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUp, upload]).then((values) => {
+    const result = [];
+    values.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        result.push({ status: val.status, value: val.value });
+      } else {
+        result.push({
+          status: val.status,
+          value: `Error: ${val.reason.message}`,
+        });
+      }
+    });
+    return result;
   });
 }
